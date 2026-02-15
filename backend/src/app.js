61b229cfdacf19c76c5e35b783ff47b133c963b9
vivekkr10 <vivekkr48027@gmail.com>
2026-02-15 23:18:20 +0530

    fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,43 +7,48 @@ import customerProductRoutes from "./routers/customer/product.route.js";
 import wishListRoutes from "./routers/customer/wishlist.route.js";
 import customerStoreRoutes from "./routers/customer/store.route.js";
 import cartRoutes from "./routers/customer/cart.route.js";
-import subscribeRoutes from "./routers/customer/subcribe&coupon.route.js"
+import subscribeRoutes from "./routers/customer/subcribe&coupon.route.js";
 
 import adminAuthRoutes from "./routers/admin/auth.route.js";
 import adminProductRoutes from "./routers/admin/product.route.js";
 import adminStoreRoutes from "./routers/admin/store.route.js";
-import adminPromationRoutes from "./routers/admin/promation.route.js"
+import adminPromationRoutes from "./routers/admin/promation.route.js";
 
 import userOrderRoutes from "./routers/order/userOrder.route.js";
-import orderRoutes from "./routers/order/order.routes.js"
-import adminOrderRoutes from "./routers/order/adminOrderRoutes.js"
+import orderRoutes from "./routers/order/order.routes.js";
+import adminOrderRoutes from "./routers/order/adminOrderRoutes.js";
 import commonSearchRoutes from "./routers/common/search.route.js";
 
 const app = express();
 
-app.use(cors({
-    origin:["http://localhost:5173", "https://g-crown.vercel.app"],
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://g-crown.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders:["Content-Type", "Authorization"],  
-}));
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Rewrite working 🚀" });
+});
 
 app.use(cookieParser());
 
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json({ limit: "100mb" }));
 
 app.use("/api/v1/customer/auth", customerAuthRoutes);
 app.use("/api/v1/customer/product", customerProductRoutes);
 app.use("/api/v1/customer/wishlist", wishListRoutes);
 app.use("/api/v1/customer/store", customerStoreRoutes);
 app.use("/api/v1/customer/cart", cartRoutes);
-app.use("/api/v1/customer/subscribe&coupon", subscribeRoutes)
+app.use("/api/v1/customer/subscribe&coupon", subscribeRoutes);
 
 app.use("/api/v1/admin/auth", adminAuthRoutes);
 app.use("/api/v1/admin/product", adminProductRoutes);
 app.use("/api/v1/admin/store", adminStoreRoutes);
-app.use("/api/v1/admin/order", adminOrderRoutes)
+app.use("/api/v1/admin/order", adminOrderRoutes);
 app.use("/api/v1/admin/prom", adminPromationRoutes);
 
 app.use("/api/v1/customer/order", orderRoutes);
